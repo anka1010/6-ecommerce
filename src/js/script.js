4f275b4e2b4b42301c249ae9edd048fed27d7600
an_lou <an.louc@gmail.com>
2023-10-18 12:48:18 +0300

    Synchronize

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,13 @@
 "use strict";
+
+// CHANGING CURRENT PRODUCT IMAGE
+const currentImage = document.querySelector("#current-image");
+const productThumbBtns = document.querySelectorAll(".product__thmb-btn");
+
+productThumbBtns.forEach((btn) =>
+  btn.addEventListener("click", () => {
+    if (!btn.classList.contains("product__thmb-btn--current")) {
+      btn.classList.toggle("product__thmb-btn--current");
+    }
+  })
+);
